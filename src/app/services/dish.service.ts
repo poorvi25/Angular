0a908c05a2114026e5631020955585cfdfbaea30
poorvi25 <poorvishrivastav80@gmail.com>
2019-06-13 13:55:53 +0530

    Single Page Applications Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -10,8 +10,9 @@ export class DishService {
   getDishes():Dish[]{
     return DISHES;
   }
+  //filter dish on basis of id
   getDish(id: string): Dish {
-  return DISHES.filter((dish) =>(dish.id===id))[0];
+  return DISHES.filter((dish) =>(dish.id=== id))[0];
   }
  getFeaturedDish(): Dish {
    return DISHES.filter((dish)=> dish.featured)[0];
