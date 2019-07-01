fc4e10bc974e45132f631e48d78f3b817467f9ad
poorvi25 <poorvishrivastav80@gmail.com>
2019-07-01 20:01:44 +0100

    animations part-1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,10 +6,25 @@ import {DishService} from '../services/dish.service';
 import{ switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from'@angular/forms';
 import { Comment} from '../shared/comment';
+import { trigger, state, animate, transition, style } from '@angular/animations';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   
@@ -33,6 +48,7 @@ export class DishdetailComponent implements OnInit {
      comment: Comment;
      //hold copy of modified dish
      dishcopy: Dish;
+     visibility = 'shown';
     //form reset to its intial value
     @ViewChild('cform') commentFormDirective;
     formErrors = {
@@ -60,8 +76,8 @@ export class DishdetailComponent implements OnInit {
     //it  will take the param value and pass it to getdish to get id
     //then it subscribe to get dish to get variable
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id']))) //fetch id
-      .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id)},
+      .pipe(switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishservice.getDish(params['id']); })) //fetch id
+      .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown';},
        errmess => this.errMess = <any>errmess ); //fetch dish from id
   }
   //to navigate other we need to know its previous id, dishid is current dish
